7fed38b0653d8f8d2b7eff8cc7979b8b62428237
lucianoreguera <regueraluciano@gmail.com>
2023-05-16 10:53:49 -0300

    Routing

---
+++ ejercicios/04-routing/src/views/BlogView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="blog">
+    <h1>My Blog</h1>
+    <ul class="blog-list">
+      <li><router-link :to="{name: 'post', params: {id: 1}}">My first Post</router-link></li>
+      <li><router-link :to="{name: 'post', params: {id: 2}}">My second Post</router-link></li>
+      <li><router-link :to="{name: 'post', params: {id: 3}}">My third Post</router-link></li>
+      <li><router-link :to="{name: 'post', params: {id: 4}}">My fourth Post</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+  .blog-list {
+    list-style-type: none;
+  }
+</style>
---
+++ ejercicios/04-routing/src/views/PostView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="blog">
+    <h1>My Post #{{ postId }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router';
+
+let postId = ref('')
+
+onMounted(() => {
+  const route = useRoute()
+  postId.value = route.params.id
+})
+
+</script>
